7115de6d8411d586d279d872e772e7d07b7a18b2
kenny5033 <dazrerx@gmail.com>
2024-12-10 08:57:22 -0500

    talks with IPP (underlying HTTP) server

--- app.py
+++ app.py
@@ -1,46 +1,47 @@
+#################################
+## Text2Type Web Interface
+## ---------
+## Runs the web interface for the Text2Type projects
+## Collects form data and sends it to the IPP Server
+## Run with 
+##   python3 app.py
+#########################
 from flask import Flask, render_template, request
-import os
+import requests
+
+APP_PORT = 8080
+IPP_SERVER_PORT = 5000
+DEBUG = True
 
 app = Flask(__name__)
 
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'POST':
-#         user_input = request.form.get('user_input')
-#         # Process the input as needed
-#         return f"You entered: {user_input}"  # Displays the input on the page
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=80)  # Runs the app on port 80
 @app.route('/')
 def index():
-    app.logger.info("Index route is called")
     return render_template('index.html')
-    
 
 @app.route('/print', methods=['POST'])
 def print_document():
     if 'file' not in request.files:
+        app.logger.error("No file uploaded")
         return "No file uploaded", 400
+
     file = request.files['file']
     if file.filename == '':
+        app.logger.error("No file selected")
         return "No selected file", 400
 
-    file_path = f'/tmp/{file.filename}'
-    file.save(file_path)
-
     try:
-        # Send file to CUPS for printing
-        os.system(f'lp {file_path}')
+        resp = requests.post(f"http://localhost:{IPP_SERVER_PORT}", files=dict(file=request.files["file"]))
+        if resp.status_code == 200:
+            app.logger.info("Request sent to IPP Server successfully")
+        else:
+            app.logger.error(f"Request to IPP Server failed {resp.status_code}")
+            return render_template("message.html", filename=file.filename, status=f"Request to IPP Server failed {resp.status_code}"), resp.status_code
     except Exception as e:
-        return f"Error during printing: {str(e)}", 500
+        app.logger.error(f"Failed to send request to IPP server: {e}")
+        return render_template("message.html", filename=file.filename, status="Failed to send request to IPP server. Perhaps it is not running?"), 500
 
-    # Optionally clean up the file after printing
-    os.remove(file_path)
-
-    return f"File '{file.filename}' successfully printed.", 200
+    return render_template("message.html", filename=file.filename, status="Successfully sent"), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=DEBUG, port=APP_PORT)
